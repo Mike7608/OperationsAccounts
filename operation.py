213fc8e1f431c8cf4b61f9b5393d1f85faabeebc
Mike Kushner <mike760807@gmail.com>
2023-08-17 13:19:14 +0300

    Добавлены докстринги и комментарии

--- operation.py
+++ operation.py
@@ -16,9 +16,16 @@ class Operation:
         return self.data[key]
 
     def get_security_account_operation_from(self):
+        """
+        Процедура шифрования и форматирования картсчета в формате XXXX XX** **** XXXX
+        :return: Счет XXXX XX** **** XXXX
+        """
         str_tmp = str(self.data['from']).split(" ")
         len_tmp = len(str_tmp)
+        # берем строку со счетом
         str_sel = str_tmp[len_tmp - 1]
+        # в счете должно быть 16 символов и более
+        # если менее то выводим строку init.STRING_FROM_IS_NONE
         if len(str_sel) > 15:
             str_tmp.pop(len_tmp - 1)
             len_acc = len(str_sel)
@@ -26,6 +33,7 @@ class Operation:
             end_str = str_sel[len_acc - init.COUNT_DISPLAY_SYMBOLS_END_STRING_FROM:]
             sec_str = init.SYMBOL_FOR_SECURITY * (len_acc - len(start_str) - len(end_str))
             str_result = start_str + sec_str + end_str
+            # делим строку на блоки по n-знаков
             n = init.COUNT_DIGITS_IN_BLOK
             chunks = [str_result[i:i + n] for i in range(0, len(str_result), n)]
             return str(" ".join(str_tmp) + " " + " ".join(chunks)).strip()
@@ -33,6 +41,10 @@ class Operation:
             return init.STRING_FROM_IS_NONE
 
     def get_security_account_operation_to(self):
+        """
+        Процедура шифрования и форматирования счета получателя в формате **XXXX
+        :return: Счет **XXXX
+        """
         str_tmp = str(self.data['to']).split(" ")
         len_tmp = len(str_tmp) - 1
         str_tmp[len_tmp] = (str(init.SYMBOL_FOR_SECURITY * 2) +
