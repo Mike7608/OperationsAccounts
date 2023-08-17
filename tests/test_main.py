7535ca6b2b0326cbad8eb7c08938199c7a427977
Mike Kushner <mike760807@gmail.com>
2023-08-17 14:32:58 +0300

    Добавлены тесты test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,15 @@
 import main
 
+
+def test_load_data():
+    data = main.load_data("operations.json")
+    assert isinstance(data, list)
+
+
+def test_not_load_data():
+    err_file = main.load_data("operations.json-0")
+    assert err_file is None
+
 # def test_get():
 #     assert arrs.get([1, 2, 3], 1, "test") == 3
 #     assert arrs.get([], 0, "test") == "test"
